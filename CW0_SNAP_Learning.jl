3b997dc17403315ffb625355c4303913b6ae7bdf
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2021-12-07 02:14:23 -0500

    which input parameters to use here?

--- CW0_SNAP_Learning.jl
+++ CW0_SNAP_Learning.jl
@@ -23,26 +23,28 @@ using PotentialLearning
 """
     gen_test_atomic_conf(D)
 
-Generate test atomic configurations
+Generate test atomic configurations.
+See https://ase.tufts.edu/chemistry/lin/images/FortranMD_TeachersGuide.pdf
 """
 function gen_test_atomic_conf(D)
     # Domain
-    box = [[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]] * 1.0u"cm"
+    d = 3.47786
+    box = [[d, 0.0, 0.0], [0.0, d, 0.0], [0.0, 0.0, d]] * 1.0u"nm"
     # Boundary conditions
-    bcs = [Periodic(), Periodic(), DirichletZero()]
+    bcs = [DirichletZero(), DirichletZero(), DirichletZero()]
     # No. of atoms per configuration
-    N = 30
+    N = 30 #864
     # No. of configurations
-    M = 20
+    M = 90
     # Element
-    c = elements[:C]
+    elem = elements[:Ar]
     # Define atomic configurations
     atomic_confs = []
     for j in 1:M
         atoms = []
         for i in 1:N
-            pos = SVector{D}(rand(D)*1.0u"cm"...)
-            atom = StaticAtom(pos,c)
+            pos = SVector{D}(rand(D)*d*1.0u"nm"...)
+            atom = StaticAtom(pos, elem)
             push!(atoms, atom)
         end
         push!(atomic_confs, FlexibleSystem(box, bcs, atoms))
@@ -57,11 +59,12 @@ D = 3; T = Float64 # TODO: discuss which parametric types are necessary, define 
 atomic_confs = gen_test_atomic_conf(D)
 
 # Generate learning data using Lennard Jones and the atomic configurations
-lj = LennardJones(1.657e-21u"J", 0.34u"nm")
+ϵ = 1.657e-21u"J"; σ = 0.34u"nm"; cutoff = 2*σ #0.765u"nm"
+lj = LennardJones(ϵ, σ, cutoff)
 data = gen_test_data(D, atomic_confs, lj)
 
 # Define target potential: SNAP
-rcutfac = 0.1; twojmax = 2
+rcutfac = cutoff.val; twojmax = 9
 inter_pot_atomic_confs = inter_pot_conf(atomic_confs) # TODO: remove after full integration with InteratomicPotentials.jl
 snap = SNAP(rcutfac, twojmax, inter_pot_atomic_confs[1]) #TODO: improve interface, do not send a conf as argument
 
@@ -71,6 +74,12 @@ lp = SmallSNAPLP(snap, inter_pot_atomic_confs, data, trainingsize = 0.8, fit = [
 # Learn :-)
 learn(lp, LeastSquaresOpt{D, T}())
 #learn(lp, QRLinearOpt{D, T}())
-#learn(lp, β_loss, NelderMeadOpt{D, T}(100))
+#learn(lp, β_loss, NelderMeadOpt{D, T}(1000))
 
+# Validation
+y_approx = lp.A_val * lp.snap.β
+max_rel_error = maximum(abs.(lp.y_val - y_approx) ./ lp.y_val)
+max_abs_error = maximum(abs.(lp.y_val - y_approx))
+println("Maximum relative error: ", max_rel_error)
+println("Maximum absolute error: ", max_abs_error)
 
