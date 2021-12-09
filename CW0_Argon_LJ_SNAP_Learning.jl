99cfc95a9e127bb26012b70a569025c7fd8fa771
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2021-12-08 20:00:08 -0500

    simple example that works

--- CW0_Argon_LJ_SNAP_Learning.jl
+++ CW0_Argon_LJ_SNAP_Learning.jl
@@ -24,47 +24,56 @@ using PotentialLearning
     gen_test_atomic_conf(D)
 
 Generate test atomic configurations.
-See https://ase.tufts.edu/chemistry/lin/images/FortranMD_TeachersGuide.pdf
 """
-function gen_test_atomic_conf(D)
+function gen_test_atomic_conf(D, σ)
     # Domain
-    d = 3.47786
-    box = [[d, 0.0, 0.0], [0.0, d, 0.0], [0.0, 0.0, d]] * 1.0u"nm"
+    σ0 = 2.0 * σ; d = 5.0*σ
+    box = [[d, 0.0u"nm", 0.0u"nm"], [0.0u"nm", d, 0.0u"nm"], [0.0u"nm", 0.0u"nm", d]]
     # Boundary conditions
     bcs = [DirichletZero(), DirichletZero(), DirichletZero()]
     # No. of atoms per configuration
-    N = 30 #864
+    N = 2
     # No. of configurations
-    M = 90
+    M = 40
     # Element
     elem = elements[:Ar]
     # Define atomic configurations
     atomic_confs = []
     for j in 1:M
         atoms = []
-        for i in 1:N
-            pos = SVector{D}(rand(D)*d*1.0u"nm"...)
-            atom = StaticAtom(pos, elem)
-            push!(atoms, atom)
-        end
+        ϕ = rand() * 2.0 * π; θ = rand() * π
+        x = (d/2.0-σ0) * cos(ϕ) * sin(θ) + d/2.0
+        y = (d/2.0-σ0) * sin(ϕ) * sin(θ) + d/2.0
+        z = (d/2.0-σ0) * cos(θ) + d/2.0
+        pos1 = SVector{D}(x, y, z)
+        atom = StaticAtom(pos1, elem)
+        push!(atoms, atom)
+        ϕ = rand() * 2.0 * π; θ = rand() * π
+        x += σ0 * cos(ϕ) * sin(θ)
+        y += σ0 * sin(ϕ) * sin(θ)
+        z += σ0 * cos(θ)
+        pos2 = SVector{D}(x, y, z)
+        atom = StaticAtom(pos2, elem)
+        push!(atoms, atom)
         push!(atomic_confs, FlexibleSystem(box, bcs, atoms))
     end
     return atomic_confs
 end
 
-# Define parametric types 
+# Define parameters
 D = 3; T = Float64 # TODO: discuss which parametric types are necessary, define a common policy for all packages 
+σ = 1.0u"nm"
 
 # Generate test atomic configurations: domain and particles (position, velocity, etc)
-atomic_confs = gen_test_atomic_conf(D)
+atomic_confs = gen_test_atomic_conf(D, σ)
 
 # Generate learning data using Lennard Jones and the atomic configurations
-ϵ = 1.657e-21u"J"; σ = 0.34u"nm"; cutoff = 2*σ #0.765u"nm"
+ϵ = 1.0u"J"; cutoff = 2.5*σ
 lj = LennardJones(ϵ, σ, cutoff)
 data = gen_test_data(D, atomic_confs, lj)
 
 # Define target potential: SNAP
-rcutfac = cutoff.val; twojmax = 9
+rcutfac = cutoff.val; twojmax = 6
 inter_pot_atomic_confs = inter_pot_conf(atomic_confs) # TODO: remove after full integration with InteratomicPotentials.jl
 snap = SNAP(rcutfac, twojmax, inter_pot_atomic_confs[1]) #TODO: improve interface, do not send a conf as argument
 
