c1e3839c436d89215839594b0a95a0aaed1ab754
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2022-04-13 14:10:49 -0400

    trying to reproduce rdf from 10.1038/s41524-020-00367-7

--- HfO2/ACE/run-md-hfo2-ace-molly.jl
+++ HfO2/ACE/run-md-hfo2-ace-molly.jl
@@ -16,21 +16,19 @@ run(`mkdir -p $experiment_path`)
 systems, energies, forces, stresses = load_data("data/a-Hfo2-300K-NVT.extxyz")
 N = length(systems[1])
 initial_system = systems[1]
-Δt = 2.5u"ps"
+Δt = 1.0u"fs"
 
 
 # Thermostat ###################################################################
 reference_temp = 300.0u"K"
-eq_thermostat = Molly.AndersenThermostat(reference_temp, Δt / 100)
+ν = 0.1 / Δt # stochastic collision frequency
+thermostat = Molly.AndersenThermostat(austrip(reference_temp), austrip(ν))
+#τNH = 20000Δt
+#thermostat = NBodySimulator.NoseHooverThermostat(austrip(reference_temp), austrip(τNH))
 
 
 # Create potential #############################################################
-n_body = 2
-max_deg = 3
-r0 = 1
-rcutoff = 5
-wL = 1
-csp = 1
+n_body = 2; max_deg = 3; r0 = 1; rcutoff = 5; wL = 1; csp = 1
 β = [-1072.1312864239615, -415.4169141829893, -71.953485409436, 1958.9574772187705,
      -281.6558117872044, 630.2363590775907, -2041.0185474208306, 240.9014951656523,
      -660.3623178315258, 267.9793847831617, 129.99873251077383, 43.761861053870035]
@@ -40,33 +38,24 @@ potential = RPI(β, rpi_params)
 function InteratomicPotentials.energy_and_force(s::AbstractSystem, p::RPI)
     B = evaluate_basis(s, rpi_params)
     dB = evaluate_basis_d(s, rpi_params)
-    e = B' * p.coefficients * 1.0u"kJ * mol^-1"
-    f = [SVector(d * p.coefficients * 1.0u"kJ * mol^-1 * nm^-1"...) for d in dB]
+    e = austrip.(B' * p.coefficients * 1u"eV")
+    f = [SVector(austrip.(d * p.coefficients .* 10u"eV/Å")...) for d in dB]
     return (; e, f)
 end
 
 
-# First stage ##################################################################
-eq_steps = 100
-eq_simulator = MollySimulator(Δt, eq_steps, coupling = eq_thermostat)
-eq_result = @time simulate(initial_system, eq_simulator, potential)
-
-
-# Second stage #################################################################
-prod_steps = 100
-prod_simulator = MollySimulator(Δt, prod_steps, t₀ = austrip(get_time(eq_result)))
-prod_result = @time simulate(get_system(eq_result), prod_simulator, potential)
+# Simulation ###################################################################
+steps = 1000 # 100_000
+simulator = MollySimulator(Δt, steps, coupling = thermostat)
+result = @time simulate(initial_system, simulator, potential)
 
 
 # Results ######################################################################
-temp = plot_temperature(eq_result, 10)
-energy = plot_energy(eq_result, 10)
-
-savefig(plot_temperature!(temp, prod_result, 10), experiment_path*"temp_hfo2_ace_molly.svg")
-savefig(plot_energy!(energy, prod_result, 10), experiment_path*"energy_hfo2_ace_molly.svg")
-
-rdf = plot_rdf(prod_result, 1.0, Int(0.95 * prod_steps))
-savefig(rdf, experiment_path*"rdf_hfo2_ace_molly_rdf.svg")
-
-visualize(prod_result, experiment_path*"hfo2_ace_molly.gif")
+savefig(plot_temperature(result, 10),
+        experiment_path*"temp_hfo2_ace_molly.svg")
+savefig(plot_energy(result, 10),
+        experiment_path*"temp_hfo2_ace_molly.svg")
+savefig(plot_rdf(result, 1.0, Int(0.95 * prod_steps)),
+        experiment_path*"rdf_hfo2_ace_molly_rdf.svg")
+visualize(result, experiment_path*"hfo2_ace_molly.gif")
 
--- HfO2/ACE/run-md-hfo2-ace-nbs.jl
+++ HfO2/ACE/run-md-hfo2-ace-nbs.jl
@@ -17,22 +17,19 @@ run(`mkdir -p $experiment_path`)
 systems, energies, forces, stresses = load_data("data/a-Hfo2-300K-NVT.extxyz")
 N = length(systems[1])
 initial_system = systems[1]
-Δt = 2.5u"fs"
+Δt = 1.0u"fs"
 
 
 # Thermostat ###################################################################
 reference_temp = 300.0u"K"
-ν = 1.0 / Δt # stochastic collision frequency
-eq_thermostat = NBodySimulator.AndersenThermostat(austrip(reference_temp), austrip(ν))
+ν = 10 / Δt # stochastic collision frequency
+thermostat = NBodySimulator.AndersenThermostat(austrip(reference_temp), austrip(ν))
+#τNH = 20000Δt
+#thermostat = NBodySimulator.NoseHooverThermostat(austrip(reference_temp), austrip(τNH))
 
 
 # Create potential #############################################################
-n_body = 2
-max_deg = 3
-r0 = 1
-rcutoff = 5
-wL = 1
-csp = 1
+n_body = 2; max_deg = 3; r0 = 1; rcutoff = 5; wL = 1; csp = 1
 β = [-1072.1312864239615, -415.4169141829893, -71.953485409436, 1958.9574772187705,
      -281.6558117872044, 630.2363590775907, -2041.0185474208306, 240.9014951656523,
      -660.3623178315258, 267.9793847831617, 129.99873251077383, 43.761861053870035]
@@ -43,40 +40,23 @@ function InteratomicPotentials.energy_and_force(s::AbstractSystem, p::RPI)
     B = evaluate_basis(s, rpi_params)
     dB = evaluate_basis_d(s, rpi_params)
     e = austrip.(B' * p.coefficients * 1u"eV")
-    f = [SVector(austrip.(d * p.coefficients .* 1u"eV/Å")...) for d in dB]
+    f = [SVector(austrip.(d * p.coefficients .* 10u"eV/Å")...) for d in dB]
     return (; e, f)
 end
 
 
-# First stage ##################################################################
-eq_steps = 100
-eq_simulator = NBSimulator(Δt, eq_steps, thermostat = eq_thermostat)
-eq_result = @time simulate(initial_system, eq_simulator, potential)
-
-
-# Second stage #################################################################
-#prod_steps = 100
-#prod_simulator = NBSimulator(Δt, prod_steps, t₀ = get_time(eq_result))
-#prod_result = @time simulate(get_system(eq_result), prod_simulator, potential)
+# Simulation ###################################################################
+steps = 500 # 100_000
+simulator = NBSimulator(Δt, steps, thermostat = thermostat)
+result = @time simulate(initial_system, simulator, potential)
 
 
 # Results ######################################################################
-
-savefig(plot_temperature(eq_result, 10),
+savefig(plot_temperature(result, 10),
         experiment_path*"temp_hfo2_ace_nbs.svg")
-savefig(plot_energy(eq_result, 10),
+savefig(plot_energy(result, 10),
         experiment_path*"energy_hfo2_ace_nbs.svg")
-savefig(plot_rdf(eq_result, 1.0, Int(0.95 * eq_steps)),
+savefig(plot_rdf(result, 1.0, Int(0.95 * steps)),
         experiment_path*"rdf_hfo2_ace_nbs_rdf.svg")
-animate(eq_result, experiment_path*"hfo2_ace_nbs.gif")
-
-#temp = plot_temperature(eq_result, 10)
-#energy = plot_energy(eq_result, 10)
-#savefig(plot_temperature!(temp, prod_result, 10),
-#        experiment_path*"temp_hfo2_ace_nbs.svg")
-#savefig(plot_energy!(energy, prod_result, 10),
-#        experiment_path*"energy_hfo2_ace_nbs.svg")
-#savefig(plot_rdf(prod_result, 1.0, Int(0.95 * prod_steps)),
-#        experiment_path*"rdf_hfo2_ace_nbs_rdf.svg")
-#animate(prod_result, experiment_path*"hfo2_ace_nbs.gif")
+#animate(result, experiment_path*"hfo2_ace_nbs.gif")
 
