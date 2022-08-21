e7237134e1fae56a0479701dec9a0e4ecf1798e5
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2022-08-21 16:09:33 -0400

    Small fix in gather results script.

--- workflows/ACE/gather-results.sh
+++ workflows/ACE/gather-results.sh
@@ -10,14 +10,16 @@ path=`pwd`
 # Header
 filename=`ls $path/$exp | tail -n1` 
 cat $path/$exp/$filename/input.csv | cut -d, -f1 | paste -s -d, | tr -d '\n' >> $exp/$results
+echo "," | tr -d '\n' >> $exp/$results
 cat $path/$exp/$filename/metrics.csv | cut -d, -f1 | paste -s -d, | tr -d '\n' >> $exp/$results
 echo "" >> $exp/$results
 
 # Content
 for currpath in $path/$exp/*/ 
 do
-  cat $currpath/input.csv | cut -d, -f2 | paste -s -d, | tr -d '\n' >> $exp/$results
-  cat $currpath/metrics.csv | cut -d, -f2 | paste -s -d, | tr -d '\n' >> $exp/$results
-  echo "" >> $exp/$results
+    cat $currpath/input.csv | cut -d, -f2 | paste -s -d, | tr -d '\n' >> $exp/$results
+    echo "," | tr -d '\n' >> $exp/$results
+    cat $currpath/metrics.csv | cut -d, -f2 | paste -s -d, | tr -d '\n' >> $exp/$results
+    echo "" >> $exp/$results
 done
 
